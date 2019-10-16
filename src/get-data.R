74fd9931e4d355dae20e0b6d8c4410f189b2582f
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-10-16 13:48:05 -0600

    added taxonomist

--- src/get-data.R
+++ src/get-data.R
@@ -61,16 +61,14 @@ get_mosiquito_data <- function(dp_id, variable_name) {
   file_out <- bind_rows(file_out)
   }
 
-mos_co2_traps <- get_mosiquito_data(dp_id='DP1.10043.001', 
-                                    variable_name='trapping')
-mos_co2_traps_sorting <- get_mosiquito_data(dp_id='DP1.10043.001', 
-                                               variable_name='sorting')
-mos_co2_traps_validation <- get_mosiquito_data(dp_id='DP1.10043.001', 
-                                               variable_name='validation')
+# Get the data for the C02 Trapping
+mos_co2_traps <- get_mosiquito_data(dp_id='DP1.10043.001', variable_name='trapping')
+mos_co2_traps_sorting <- get_mosiquito_data(dp_id='DP1.10043.001', variable_name='sorting')
+mos_co2_traps_taxonomist <- get_mosiquito_data(dp_id='DP1.10043.001', variable_name='TaxonomistIDProcessed')
 
-mos_pathogen_pooling <- get_mosiquito_data(dp_id='DP1.10041.001', 
-                                           variable_name='pathogenpooling')
-mos_pathogen_results <- get_mosiquito_data(dp_id='DP1.10041.001',
-                                           variable_name='pathogenresults')
-mos_pathogen_validation<- get_mosiquito_data(dp_id='DP1.10041.001', 
-                                             variable_name='validation')
+mos_co2_traps_validation <- get_mosiquito_data(dp_id='DP1.10043.001', variable_name='validation')
+
+# Get the data for mosiquto pathogens
+mos_pathogen_pooling <- get_mosiquito_data(dp_id='DP1.10041.001', variable_name='pathogenpooling')
+mos_pathogen_results <- get_mosiquito_data(dp_id='DP1.10041.001', variable_name='pathogenresults')
+mos_pathogen_validation<- get_mosiquito_data(dp_id='DP1.10041.001', variable_name='validation')
