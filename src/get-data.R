72a896686e04fcb743c06f6d08afdd8496ed2312
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-10-16 13:30:25 -0600

    added some error handling

--- src/get-data.R
+++ src/get-data.R
@@ -27,30 +27,38 @@ get_mosiquito_data <- function(dp_id, variable_name) {
   # Compile a list of all the urls
   file_list <- unlist(urls$data$siteCodes$availableDataUrls)
   
-  file_out <- do.call(rbind, pblapply(file_list, function(x, variable_name) {
-    # Packages needed to be pushed to each core for parallel processing
-    packages <- c('tidyverse', 'neonUtilities', 'httr', 'jsonlite', 'devtools', 'downloader', 'geoNEON', 'janitor', 'pbapply')
-    invisible(lapply(packages, require, character.only = TRUE, quietly = TRUE))
-    
-    # Makes sure the numbers displayed are accurate in the tibble
-    options(pillar.sigfig = 8)
-    
-    splitter <- x %>%
-      str_split(., '/') %>%
-      unlist()
-    
-    tmp_files <- fromJSON(
-      content(GET(x), as="text"), 
-      simplifyDataFrame=TRUE, flatten=TRUE)
-    
-    mos_trapping <- as_tibble(read.delim(
-      tmp_files$data$files$url[first(grep(variable_name, tmp_files$data$files$name))], 
-      sep=","))
-    
-  }, 
+  file_out <- pblapply(file_list, function(x, variable_name) {
+    tryCatch({
+      # Packages needed to be pushed to each core for parallel processing
+      packages <- c('tidyverse', 'neonUtilities', 'httr', 'jsonlite', 'devtools', 'downloader', 'geoNEON', 'janitor', 'pbapply')
+      invisible(lapply(packages, require, character.only = TRUE, quietly = TRUE))
+      
+      # Makes sure the numbers displayed are accurate in the tibble
+      options(pillar.sigfig = 8)
+      
+      splitter <- x %>%
+        str_split(., '/') %>%
+        unlist()
+      
+      tmp_files <- fromJSON(
+        content(GET(x), as="text"), 
+        simplifyDataFrame=TRUE, flatten=TRUE)
+      
+      tmp_files <- as_tibble(read.delim(
+        tmp_files$data$files$url[first(grep(variable_name, tmp_files$data$files$name))], 
+        sep=","))
+      },
+      error = function(e) {
+        file_out <<- NULL
+      }
+    )
+  },
   variable_name=variable_name,
   cl=cl
-    ))
+    )
+  file_out <- file_out[file_out != "NULL"]
+  # Bind all list elements into one dataframe for output
+  file_out <- bind_rows(file_out)
   }
 
 mos_co2_traps <- get_mosiquito_data(dp_id='DP1.10043.001', 
@@ -62,7 +70,7 @@ mos_co2_traps_validation <- get_mosiquito_data(dp_id='DP1.10043.001',
 
 mos_pathogen_pooling <- get_mosiquito_data(dp_id='DP1.10041.001', 
                                            variable_name='pathogenpooling')
-mos_pathogen_results <- get_mosiquito_data(dp_id='DP1.10041.001', 
+mos_pathogen_results <- get_mosiquito_data(dp_id='DP1.10041.001',
                                            variable_name='pathogenresults')
 mos_pathogen_validation<- get_mosiquito_data(dp_id='DP1.10041.001', 
                                              variable_name='validation')
