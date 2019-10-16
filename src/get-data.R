9db215a593896f8f961383b083187a65941ef2e6
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-10-16 12:58:38 -0600

    generalized function, added dataframe for traps and pathogens

--- src/get-data.R
+++ src/get-data.R
@@ -11,41 +11,56 @@
 packages <- c('tidyverse', 'neonUtilities', 'httr', 'jsonlite', 'devtools', 'downloader', 'geoNEON', 'janitor', 'pbapply')
 invisible(lapply(packages, library, character.only = TRUE, quietly = TRUE))
 
-# Get the mosquito count data
-urls <- fromJSON(
-  content(GET("http://data.neonscience.org/api/v0/products/DP1.10043.001"), as="text"), 
-  simplifyDataFrame=TRUE, flatten=TRUE) 
-
-# Compile a list of all the urls
-file_list <- unlist(urls$data$siteCodes$availableDataUrls)
-
 # Spin-up for parallel processing
 cl <- parallel::makeCluster(getOption("cl.cores", parallel::detectCores()))
 # If error or crash, closes all clusters
 on.exit(parallel::stopCluster(cl))
 
 # Main function
-files <- do.call(rbind, pblapply(file_list, function(x) {
+get_mosiquito_data <- function(dp_id, variable_name) {
   
-  # Packages needed to be pushed to each core for parallel processing
-  packages <- c('tidyverse', 'neonUtilities', 'httr', 'jsonlite', 'devtools', 'downloader', 'geoNEON', 'janitor', 'pbapply')
-  invisible(lapply(packages, require, character.only = TRUE, quietly = TRUE))
+  # Get the mosquito count data
+  urls <- fromJSON(
+    content(GET(paste0("http://data.neonscience.org/api/v0/products/", dp_id)), as="text"), 
+    simplifyDataFrame=TRUE, flatten=TRUE) 
   
-  # Makes sure the numbers displayed are accurate in the tibble
-  options(pillar.sigfig = 8)
+  # Compile a list of all the urls
+  file_list <- unlist(urls$data$siteCodes$availableDataUrls)
   
-      splitter <- x %>%
-        str_split(., '/') %>%
-        unlist()
-      
-      tmp_files <- fromJSON(
-        content(GET(x), as="text"), 
-        simplifyDataFrame=TRUE, flatten=TRUE)
-      
-      mos_trapping <- as_tibble(read.delim(
-        tmp_files$data$files$url[first(grep('trapping', tmp_files$data$files$name))], 
-        sep=","))
-      }
-  , 
-cl=cl
-))
+  file_out <- do.call(rbind, pblapply(file_list, function(x, variable_name) {
+    # Packages needed to be pushed to each core for parallel processing
+    packages <- c('tidyverse', 'neonUtilities', 'httr', 'jsonlite', 'devtools', 'downloader', 'geoNEON', 'janitor', 'pbapply')
+    invisible(lapply(packages, require, character.only = TRUE, quietly = TRUE))
+    
+    # Makes sure the numbers displayed are accurate in the tibble
+    options(pillar.sigfig = 8)
+    
+    splitter <- x %>%
+      str_split(., '/') %>%
+      unlist()
+    
+    tmp_files <- fromJSON(
+      content(GET(x), as="text"), 
+      simplifyDataFrame=TRUE, flatten=TRUE)
+    
+    mos_trapping <- as_tibble(read.delim(
+      tmp_files$data$files$url[first(grep(variable_name, tmp_files$data$files$name))], 
+      sep=","))
+    
+  }, 
+  variable_name=variable_name,
+  cl=cl
+    ))
+  }
+
+mos_co2_traps <- get_mosiquito_data(dp_id='DP1.10043.001', 
+                                    variable_name='_trapping')
+mos_co2_traps_validation <- get_mosiquito_data(dp_id='DP1.10043.001', 
+                                               variable_name='_validation')
+
+mos_pathogen_pooling <- get_mosiquito_data(dp_id='DP1.10041.001', 
+                                   variable_name='pathogenpooling')
+mos_pathogen_results <- get_mosiquito_data(dp_id='DP1.10041.001', 
+                                               variable_name='pathogenresults')
+mos_pathogen_validation<- get_mosiquito_data(dp_id='DP1.10041.001', 
+                                           variable_name='v')
